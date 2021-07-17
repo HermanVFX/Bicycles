a6c05919c16f0cc95f30a415fc2955d98ce8f48e
HermanMR <hermanvfx@outlook.com>
2021-07-17 11:15:11 +0300

    Правки js

--- source/js/script.js
+++ source/js/script.js
@@ -1,53 +1,57 @@
 'use strict';
-const menu = document.querySelector('.header__menu');
-const menuBtn = document.querySelector('.header__menu-button');
-const contactName= document.getElementById('name');
-const contactPhone= document.getElementById('phone');
-const page = document.querySelector('.page')
-const menuItem = document.querySelectorAll('.header__menu-item');
+var menuMobile = document.querySelector('.header__menu');
+var menuBtn = document.querySelector('.header__menu-button');
+var page = document.querySelector('.page');
+var menuItem = document.querySelectorAll('.header__menu-item');
+var contactName = document.getElementById('name');
+var contactPhone = document.getElementById('phone');
 // В случае загрузки JS добавит кнопку меню
 function onLoadPage() {
   menuBtn.classList.add('header__menu-button--close');
-  menu.classList.add('header__menu--close');
-}
-// Нажатие на кнопку открытия/закрытия меню
-menuBtn.addEventListener('click', function () {
-  if (menu.classList.contains('header__menu--close')) {
-    menu.classList.remove('header__menu--close');
-    menu.classList.add('header__menu--open');
-    menuBtn.classList.remove('header__menu-button--close');
-    menuBtn.classList.add('header__menu-button--open');
-    page.classList.add('overlay');
-  } else {
-    menu.classList.add('header__menu--close');
-    menu.classList.remove('header__menu--open');
+  menuMobile.classList.add('header__menu--close');
+  // Нажатие на кнопку открытия/закрытия меню
+  menuBtn.addEventListener('click', function () {
+    if (menuMobile.classList.contains('header__menu--close')) {
+      menuMobile.classList.remove('header__menu--close');
+      menuMobile.classList.add('header__menu--open');
+      menuBtn.classList.remove('header__menu-button--close');
+      menuBtn.classList.add('header__menu-button--open');
+      page.classList.add('overlay');
+    } else {
+      menuMobile.classList.add('header__menu--close');
+      menuMobile.classList.remove('header__menu--open');
+      menuBtn.classList.remove('header__menu-button--open');
+      menuBtn.classList.add('header__menu-button--close');
+      page.classList.remove('overlay');
+    }
+  });
+
+  // Нажатие на пункты меню (закрытие меню)
+  menuItem.forEacah(addEventListener('click', function () {
+    menuMobile.classList.add('header__menu--close');
+    menuMobile.classList.remove('header__menu--open');
     menuBtn.classList.remove('header__menu-button--open');
     menuBtn.classList.add('header__menu-button--close');
     page.classList.remove('overlay');
-  }
-});
+    return;
+  }));
+}
 // Валидация формы
 function contactSaveLocalStorage() {
-  if (contactPhone.checkValidity() ==! false && contactName.checkValidity() ==! false) {
-  localStorage.setItem("email", contactName.value);
-  localStorage.setItem("phone", contactPhone.value);
-  alert ('Данные отправлены');
-} else {
+  if (contactPhone.checkValidity() === true && contactName.checkValidity() === true) {
+    localStorage.setItem('email', contactName.value);
+    localStorage.setItem('phone', contactPhone.value);
+  } else {
     contactPhone.classList.add('outline');
     contactName.classList.add('outline');
   }
-};
-// Нажатие на пункты меню (закрытие меню)
-menuItem.forEach(b=>b.addEventListener('click', function() {
-  menu.classList.add('header__menu--close');
-  menu.classList.remove('header__menu--open');
-  menuBtn.classList.remove('header__menu-button--open');
-  menuBtn.classList.add('header__menu-button--close');
-  page.classList.remove('overlay');
-  return;
-}));
+}
 // Не даю ввести в поле телефон ничего кроме цифр
 function numValidation(input) {
-  const value = input.value;
+  var value = input.value;
   input.value = value.replace(/\D/g, '');
-};
+}
+
+onLoadPage();
+contactSaveLocalStorage();
+numValidation();
